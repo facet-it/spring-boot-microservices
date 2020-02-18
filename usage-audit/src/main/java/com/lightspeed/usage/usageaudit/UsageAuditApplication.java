bb08c9c05e9cc7523e115e769219fd18584b19d7
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2020-02-18 16:35:13 +0100

    adjusted the git ignore file

---
+++ usage-audit/src/main/java/com/lightspeed/usage/usageaudit/UsageAuditApplication.java
@@ -0,0 +1,13 @@
+package com.lightspeed.usage.usageaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageAuditApplication.class, args);
+	}
+
+}
---
+++ usage-audit/src/test/java/com/lightspeed/usage/usageaudit/UsageAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightspeed.usage.usageaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
