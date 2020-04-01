d5d942e4fc2813e320d4be6c916dd85af95fe9d9
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2020-04-01 10:49:58 +0200

    first commit, updating the gitignore file

---
+++ redis/poc/src/main/java/com/lightspeed/example/poc/RedisPublisher.java
@@ -0,0 +1,5 @@
+package com.lightspeed.example.poc;
+
+public class RedisPublisher {
+
+}
---
+++ redis/test/src/test/java/com/lightspeed/redis/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightspeed.redis.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
