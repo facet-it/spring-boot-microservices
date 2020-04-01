15ec81afe887b3069bdd759ed1dbd991c702205d
Nicholas Ocket <nicholasocket@C02SY03ZFVH4-Nicholas-Ocket.local>
2020-04-01 14:56:48 +0200

    initial setup

---
+++ redis/worker/src/main/java/com/lightspeed/redis/test/worker/spring/BeanConfig.java
@@ -0,0 +1,29 @@
+package com.lightspeed.redis.test.worker.spring;
+
+import org.redisson.Redisson;
+import org.redisson.api.RTopic;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    public Config getConfig() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+        return config;
+    }
+
+    @Bean
+    public RedissonClient getRedisClient() {
+        RedissonClient client =  Redisson.create(getConfig());
+        return client;
+    }
+
+    @Bean
+    public RTopic getRTopic(RedissonClient client) {
+        return client.getTopic("chat");
+    }
+}
